4b274ac24ebf81e1155dc7090901c49bbd7bf01d
Oceankingdom <24520357@gm.uit.edu.vn>
2025-11-20 22:19:14 +0700

    Them Models(Room) và Controller

---
+++ API/Models/Room.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace API.Models
+{
+    public class Room
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString(); // Tự sinh ID ngẫu nhiên
+        public string Name { get; set; } // Tên phòng
+        public string Status { get; set; } = "Waiting"; // Trạng thái: Waiting/Playing
+        public int PlayerCount { get; set; } = 0;
+    }
+}
